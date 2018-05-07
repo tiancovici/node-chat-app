941acef492afc6e374938d188b4e2ec454660f3a
tiancovici <tiancovici@gmail.com>
2018-05-07 14:52:52 -0400

    Add connection event listeners

--- server/server.js
+++ server/server.js
@@ -1,12 +1,26 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 let port = process.env.PORT || 4200;
 let app = express();
+let server = http.createServer(app)
+let io = socketIO(server, {});
 
 app.use(express.static(publicPath));
 
-app.listen(port, ()=> {
+io.on('connection', (socket)=> {
+  console.log('New User connected');
+  io.on('disconnected', (socket)=> {
+
+    console.log('User disconnected');
+  });
+});
+
+
+
+server.listen(port, ()=> {
   console.log(`Started on port ${port}`);
 });
