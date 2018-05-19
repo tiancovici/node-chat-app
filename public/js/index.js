0e66f25452dff1767a0059f9a9e7088df2c77dd9
tiancovici <tiancovici@gmail.com>
2018-05-19 13:29:47 -0400

    add styling

--- public/js/index.js
+++ public/js/index.js
@@ -40,11 +40,14 @@ socket.on('newLocationMessage', function (message) {
 
 jQuery('#message-form').on('submit', function(e) {
   e.preventDefault();
+
+  let messageTextBox = jQuery('[name=message]')
+
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextBox.val()
   }, function(data) {
-
+    messageTextBox.val('');
   });
 });
 
@@ -54,12 +57,16 @@ locationButton.on('click', function(){
     return alert('Gelocation not supported by your browser.');
   }
 
+  locationButton.attr('disabled', 'disabled');
+
   navigator.geolocation.getCurrentPosition(function(position){
+    locationButton.removeAttr('disabled');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, function() {
+    locationButton.removeAttr('disabled').text('Send location');
     alert('Unable to fetch location')
-  })
+  });
 });
